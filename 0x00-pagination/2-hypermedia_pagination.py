337343a823be5c6b258b54e2c1827cb2cef9014b
DanielJohn17 <danielyohannes38@gmail.com>
2024-09-02 20:14:47 +0300

    finished task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+'''Simple pagination'''
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''Return a tuple of size two containing a start index and an end index'''
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    '''Server class to paginate a database of popular baby names.
+    '''
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        '''Cached dataset
+        '''
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Return the appropriate page of the dataset'''
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        start_idx, end_idx = index_range(page=page, page_size=page_size)
+        data = self.dataset()
+
+        if start_idx > len(data):
+            return []
+        return data[start_idx:end_idx]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''Return a dictionary containing the following key-value pairs'''
+        page_data = self.get_page(page, page_size)
+        start_idx, end_idx = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end_idx < len(self.__dataset) else None,
+            'prev_page': page - 1 if start_idx > 0 else None,
+            'total_pages': total_pages
+        }
+        return page_info
