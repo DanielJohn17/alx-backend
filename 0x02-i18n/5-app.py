ea90ecc896b212c677d5827b0ca656091d31b6d9
DanielJohn17 <danielyohannes38@gmail.com>
2024-09-11 17:57:12 +0300

    finished task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" 4. Force locale with URL parameter """
+from flask import Flask, request, render_template, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config:
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user() -> Union[Dict, None]:
+    """ Get user from request """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+@app.before_request
+def before_request() -> None:
+    """ Get user and set it as a global """
+    user = get_user()
+    g.user = user
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale from request """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def hello_holberton() -> str:
+    """ Display "Hello World!" """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
